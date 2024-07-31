b521e1889b8037c3e51e6818d4dcba9d7bd6d83b
Zhi Jian Tiow <aaron.tiow.99@gmail.com>
2024-07-31 15:39:43 +0100

    End of section 7

---
+++ API/Controllers/BuggyController.cs
@@ -0,0 +1,13 @@
+﻿using API.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+
+public class BuggyController(DataContext dataContext) : BaseApiController
+{
+    [HttpGet("auth")]
+    public ActionResult<string> GetAuth()
+    {
+        return "secret text";
+    }
+}
